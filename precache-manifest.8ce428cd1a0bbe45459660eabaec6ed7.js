e5cf06d56b8117531c018efd6843faf22c9deb1c
syarifmaulana084 <syarifmaulana@gmail.com>
2020-08-22 21:43:58 +0700

    memperbarui tampilan

---
+++ precache-manifest.8ce428cd1a0bbe45459660eabaec6ed7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1211ecdb0f0e21fe7ae4d58a98cad059",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be4391a3b7521441dcda",
+    "url": "/static/css/main.b9c97341.chunk.css"
+  },
+  {
+    "revision": "c9ac880b8b8d44226d10",
+    "url": "/static/js/2.f6ec501e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f6ec501e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be4391a3b7521441dcda",
+    "url": "/static/js/main.754403b9.chunk.js"
+  },
+  {
+    "revision": "28ba23d0d3ba8e4d2ed0",
+    "url": "/static/js/runtime-main.cdda94e0.js"
+  },
+  {
+    "revision": "ad94bd9017e99f9c6f1b069bfa2beeb8",
+    "url": "/static/media/p1.ad94bd90.jpg"
+  },
+  {
+    "revision": "fae6fe851dc616d7954b7deb374d9552",
+    "url": "/static/media/p2.fae6fe85.jpg"
+  },
+  {
+    "revision": "126880ce19989d9829c2a47b0f8f085b",
+    "url": "/static/media/p3.126880ce.jpg"
+  },
+  {
+    "revision": "5ce479b959d4136b5f8e405e7fd72146",
+    "url": "/static/media/product1.5ce479b9.jpg"
+  }
+]);
